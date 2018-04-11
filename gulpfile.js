657798ea073c887e0ec7baf716d09a7994881c17
paul18design <pablo_beltran15@outlook.com>
2018-04-11 10:31:12 -0500

    Subir Docs

--- gulpfile.js
+++ gulpfile.js
@@ -9,31 +9,31 @@ var browserSync = require('browser-sync').create();
 
 gulp.task('default', ['css', 'javascript'], function(){
     browserSync.init({
-        server: "./app"
+        server: "./docs"
     });
 
-    gulp.watch("app/js/*.js", ["javascript"]).on('change', browserSync.reload);
+    gulp.watch("docs/js/*.js", ["javascript"]).on('change', browserSync.reload);
     gulp.watch('scss/**/*.scss', ['css']);
-    gulp.watch("app/*.html").on('change', browserSync.reload);
+    gulp.watch("docs/*.html").on('change', browserSync.reload);
     gulp.watch("./*.html", ["html"]);
 });
 
 gulp.task('html', function() {
     return gulp.src('./*.html')
         .pipe(htmlmin({collapseWhitespace: true}))
-        .pipe(gulp.dest('app'));
+        .pipe(gulp.dest('docs'));
 });
 
 gulp.task('images', function() {
         gulp.src('img/*')
             .pipe(imagemin())
-            .pipe(gulp.dest('app/img'));
+            .pipe(gulp.dest('docs/img'));
 });
 
 gulp.task('javascript', function() {
-        gulp.src('app/js/*.js')
+        gulp.src('docs/js/*.js')
             .pipe(uglify())
-            .pipe(gulp.dest('app/js/dist'));
+            .pipe(gulp.dest('docs/js/dist'));
 });
 
 gulp.task('css', function(){
@@ -44,7 +44,7 @@ gulp.task('css', function(){
             browsers: ['last 2 versions'],
             cascade: false
         }))
-        .pipe(gulp.dest('app/css'))
+        .pipe(gulp.dest('docs/css'))
         .pipe(browserSync.stream());
 });
 
